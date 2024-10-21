1f233a63d2396dd9a1270762c5184f387a842cd0
hafidjs <afidzpratama@gmail.com>
2024-10-21 12:45:31 +0700

    Coroutine Test Many

--- app/src/test/kotlin/org/example/CoroutineTest.kt
+++ app/src/test/kotlin/org/example/CoroutineTest.kt
@@ -5,6 +5,8 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
+import java.util.Date
+import kotlin.concurrent.thread
 
 class CoroutineTest {
 
@@ -24,4 +26,35 @@ class CoroutineTest {
         }
         println("SELESAI")
     }
+
+    @Test
+    fun testThread() {
+        repeat(10_000) {
+            thread {
+                Thread.sleep(1_000)
+                println("Done $it : ${Date()}")
+            }
+        }
+
+        println("Waiting")
+        Thread.sleep(3_000)
+        println("Finish")
+    }
+
+    @Test
+    fun testCoroutineMany() {
+        repeat(10_000) {
+            GlobalScope.launch {
+                delay(1_000)
+                println("Done $it : ${Date()} : ${Thread.currentThread().name}")
+            }
+        }
+
+        println("Waiting")
+        runBlocking {
+            delay(3_000)
+        }
+        println("Finish")
+    }
+
 }
