d31ad6f054250d2db2f17bd0630b8bfd75985b52
hafidjs <afidzpratama@gmail.com>
2024-10-21 14:57:09 +0700

    Timeouts

---
+++ app/src/test/kotlin/org/example/Timeout.kt
@@ -0,0 +1,21 @@
+package org.example
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+
+    println("Main program starts: ${Thread.currentThread().name}")
+
+    val result: String? = withTimeoutOrNull(2000)  {
+            for (i in 0..500) {
+                print("$i.")
+                delay(500)
+            }
+
+        "I am done"
+    }
+
+    print("Result $result")
+
+    println("\nMain program ends : ${Thread.currentThread().name}")
+}
