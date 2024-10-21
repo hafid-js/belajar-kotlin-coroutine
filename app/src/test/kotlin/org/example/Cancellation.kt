83bbe93af4833327d59421d2b0d0ecbab6650b01
hafidjs <afidzpratama@gmail.com>
2024-10-21 14:48:24 +0700

    Handle Exception

--- app/src/test/kotlin/org/example/Cancellation.kt
+++ app/src/test/kotlin/org/example/Cancellation.kt
@@ -6,18 +6,25 @@ fun main() = runBlocking {
     println("Main program starts : ${Thread.currentThread().name}")
 
     val job: Job = launch(Dispatchers.Default) {
-        for (i in 0..500) {
-            if (!isActive) {
-//                break
-                return@launch
-            }
-            print("$i.")
-            Thread.sleep(1)
-        }
+       try {
+           for (i in 0..500) {
+               print("$i.")
+               delay(5)
+           }
+       } catch (ex: CancellationException) {
+           print("\nException caught safely : ${ex.message}")
+       } finally {
+           withContext(NonCancellable) {
+               delay(1000)
+               print("\nClose resources in finally")
+           }
+
+       }
 }
 
      delay(10)
-    job.cancelAndJoin()
+    job.cancel(CancellationException("My own crash message"))
+    job.join()
     println("\nMain program ends : ${Thread.currentThread().name}")
 
 
